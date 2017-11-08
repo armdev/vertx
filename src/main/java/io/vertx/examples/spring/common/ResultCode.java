7474f7cf30bfab8bbfeb7317d82dab9a749d9534
orangehs <1191066931@qq.com>
2017-11-08 19:41:06 +0800

    增加swagger api文档描述

--- src/main/java/io/vertx/examples/spring/common/ResultCode.java
+++ src/main/java/io/vertx/examples/spring/common/ResultCode.java
@@ -5,7 +5,7 @@ package io.vertx.examples.spring.common;
  */
 public enum ResultCode {
     SUCCESS(0),//成功
-    FAIL(400),//失败
+    FAIL(1),//失败
     UNAUTHORIZED(401),//未认证（签名错误）
     NOT_FOUND(404),//接口不存在
     INTERNAL_SERVER_ERROR(500);//服务器内部错误
---
+++ src/main/java/io/vertx/examples/spring/controller/PageInfoBeen.java
@@ -0,0 +1,27 @@
+package io.vertx.examples.spring.controller;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+
+public class PageInfoBeen {
+
+    @ApiParam(value = "当前页", required = true)
+    private Integer nowPage;
+    @ApiModelProperty(value = "每页大小", required = true)
+    private Integer pageSize;
+    //省略 set get
+	public Integer getNowPage() {
+		return nowPage;
+	}
+	public void setNowPage(Integer nowPage) {
+		this.nowPage = nowPage;
+	}
+	public Integer getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+    
+    
+}
